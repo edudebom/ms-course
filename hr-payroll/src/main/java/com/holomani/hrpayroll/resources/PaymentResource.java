60418a1dab609387794764bc338c7cc4396f3beb
Eduardo Debom <eduardodebom@hotmail.com>
2021-01-27 20:25:55 -0300

    payroll

---
+++ hr-payroll/src/main/java/com/holomani/hrpayroll/resources/PaymentResource.java
@@ -0,0 +1,25 @@
+package com.holomani.hrpayroll.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.holomani.hrpayroll.entities.Payment;
+import com.holomani.hrpayroll.services.PaymentService;
+
+@RestController
+@RequestMapping(value ="/payments")
+public class PaymentResource {
+
+	@Autowired
+	private PaymentService service;
+	
+	@GetMapping(value = "/{workerId}/days/{days}" )
+	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
+		Payment payment = service.getPayment(workerId, days);
+		return ResponseEntity.ok(payment);
+	}
+}
