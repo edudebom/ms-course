90ac18e8ca4fc9ada3bba3698054087f4bfd700c
Eduardo Debom <eduardodebom@hotmail.com>
2021-01-27 22:21:41 -0300

    feign client

--- hr-payroll/src/main/java/com/holomani/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/holomani/hrpayroll/services/PaymentService.java
@@ -10,22 +10,17 @@ import org.springframework.web.client.RestTemplate;
 
 import com.holomani.hrpayroll.entities.Payment;
 import com.holomani.hrpayroll.entities.Worker;
+import com.holomani.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		
-		Map<String, String> uriVariable = new HashMap<>();
-		uriVariable.put("id", "" + workerId);
-		
-		Worker worker = restTemplate.getForObject(workerHost + "workers/{id}" , Worker.class, uriVariable);
+			
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
